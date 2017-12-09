30ef1c6a91cfe9857f765f559253ff4172535f93
jayant sarkar <codejayant@gmail.com>
2017-12-09 14:54:18 -0800

    redux demo : WIP

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function  () {
+  return  [
+    { title: 'Javascript works' },
+    { title: 'beauty in a beast' },
+    { title: 'falling in the ocean' },
+    { title: 'revolving time' }
+  ]
+}
